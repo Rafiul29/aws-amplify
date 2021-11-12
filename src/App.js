143068ee1bd2c818740b2828dc4fd71f84272cb7
Rafiul29 <rafiulislam4472@gmail.com>
2021-11-12 20:09:38 +0600

    ampliy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import P from './P';
+
+function App() {
+  return (
+    <div className="App">
+      <P/>
+ 
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/P.js
@@ -0,0 +1,11 @@
+
+
+function p(){
+
+    return(
+        <div>
+            <h1>AWS Severless Amplify</h1>
+        </div>
+    );
+}
+export default p;
